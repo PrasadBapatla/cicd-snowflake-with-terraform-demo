0c8693c0dc4beebdad65b46eba7d3897b1ebcc92
Bapatla, Prasad <PBapatla@ap.org>
2026-04-28 11:05:55 -0400

    fixed the format

--- staging/main.tf
+++ staging/main.tf
@@ -7,17 +7,17 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "prasad-snowflake-demo"
-    key            = "terraform-staging.tfstate"
-    region         = "us-east-1"
+    bucket = "prasad-snowflake-demo"
+    key    = "terraform-staging.tfstate"
+    region = "us-east-1"
     # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
     # dynamodb_table = "terraform-state-lock-table"
-    encrypt        = true
-    assume_role= { 
-       role_arn  = "arn:aws:iam::222259241209:role/prasad-snowflake-demo-s3"
-       session_name = "terraform-session" # Optional
+    encrypt = true
+    assume_role = {
+      role_arn     = "arn:aws:iam::222259241209:role/prasad-snowflake-demo-s3"
+      session_name = "terraform-session" # Optional
     }
-   
+
   }
 }
 
