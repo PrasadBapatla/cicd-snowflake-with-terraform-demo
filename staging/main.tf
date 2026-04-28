778045abe87c8a210d35600b04308f52cf96f396
Bapatla, Prasad <PBapatla@ap.org>
2026-04-28 10:00:02 -0400

    fixed the role_arn tag

--- staging/main.tf
+++ staging/main.tf
@@ -13,7 +13,11 @@ terraform {
     # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
     # dynamodb_table = "terraform-state-lock-table"
     encrypt        = true
-    role_arn       = "arn:aws:iam::222259241209:role/prasad-snowflake-demo-s3"
+    assume_role= { 
+       role_arn  = "arn:aws:iam::222259241209:role/prasad-snowflake-demo-s3"
+       session_name = "terraform-session" # Optional
+    }
+   
   }
 }
 
