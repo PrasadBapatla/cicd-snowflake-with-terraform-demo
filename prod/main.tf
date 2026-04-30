524b8a72f93191e0b859919d2304bb3c0b9e01f5
Bapatla, Prasad <PBapatla@ap.org>
2026-04-30 10:06:40 -0400

    Prod Config Changes

--- prod/main.tf
+++ prod/main.tf
@@ -7,20 +7,29 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "prasad-snowflake-demo"
-    key            = "terraform-prod.tfstate"
-    region         = "us-east-1"
+    bucket = "prasad-snowflake-demo"
+    key    = "terraform-prod.tfstate"
+    region = "us-east-1"
     # Optional DynamoDB for state locking. See https://developer.hashicorp.com/terraform/language/settings/backends/s3 for details.
     # dynamodb_table = "terraform-state-lock-table"
-    encrypt        = true
-    assume_role= { 
-       role_arn  = "arn:aws:iam::222259241209:role/prasad-snowflake-demo-s3"
-       session_name = "terraform-session" # Optional
+    encrypt = true
+    assume_role = {
+      role_arn     = "arn:aws:iam::222259241209:role/prasad-snowflake-demo-s3"
+      session_name = "terraform-session" # Optional
     }
   }
 }
 
-provider "snowflake" {}
+provider "snowflake" {
+  account_name      = var.snowflake_account_name
+  organization_name = var.snowflake_organization_name
+
+  preview_features_enabled = [
+    "snowflake_table_resource"
+  ]
+
+}
+
 
 module "snowflake_resources" {
   source              = "../modules/snowflake_resources"
